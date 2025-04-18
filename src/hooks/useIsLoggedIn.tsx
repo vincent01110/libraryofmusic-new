191f90a1950cd303dd80cde390a777baad7dc11f
vincent01110 <96280211+vincent01110@users.noreply.github.com>
2025-04-18 10:45:05 +0200

    Revert "Carousel"

--- src/hooks/useIsLoggedIn.tsx
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers';
-
-export async function useIsLoggedIn(): Promise<boolean> {
-    const store = await cookies();
-    const isLoggedIn = store.get('API_TOKEN');
-    
-    return !!isLoggedIn?.value;
-}
